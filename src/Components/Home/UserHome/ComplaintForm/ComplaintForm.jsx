c5268f7b68f5c852497c12347bd4632d29d8155b
Rubz07 <rubinsiby@gmail.com>
2021-12-04 23:32:32 +0530

    complaint form design added need to edit

--- src/Components/Home/UserHome/ComplaintForm/ComplaintForm.jsx
+++ src/Components/Home/UserHome/ComplaintForm/ComplaintForm.jsx
@@ -2,52 +2,79 @@ import React from "react";
 import "./ComplaintForm.css";
 function ComplaintForm() {
   return (
-    <div className="newUser">
-      <h1 className="newUserTitle">Post your complaint</h1>
-      <form className="newUserForm">
-        <div className="newUserItem">
-          <label>Username</label>
-          <input type="text" placeholder="john" />
-        </div>
-        <div className="newUserItem">
-          <label>Full Name</label>
-          <input type="text" placeholder="John Smith" />
-        </div>
-        <div className="newUserItem">
-          <label>Email</label>
-          <input type="email" placeholder="john@gmail.com" />
-        </div>
-        <div className="newUserItem">
-          <label>Password</label>
-          <input type="password" placeholder="password" />
-        </div>
-        <div className="newUserItem">
-          <label>Phone</label>
-          <input type="text" placeholder="+1 123 456 78" />
-        </div>
-        <div className="newUserItem">
-          <label>Address</label>
-          <input type="text" placeholder="New York | USA" />
-        </div>
-        <div className="newUserItem">
-          <label>Gender</label>
-          <div className="newUserGender">
-            <input type="radio" name="gender" id="male" value="male" />
-            <label for="male">Male</label>
-            <input type="radio" name="gender" id="female" value="female" />
-            <label for="female">Female</label>
-            <input type="radio" name="gender" id="other" value="other" />
-            <label for="other">Other</label>
+    <div className="complaintForm">
+      <div className="form-title">Complaint</div>
+      <form>
+        <div className="complaint-details">
+          <div className="input-box">
+            <span className="details">Select category</span>
+            <select className="drp-control" data-flag="true">
+              <option value="  " selected>
+                Select Category
+              </option>
+              <option>Bill Related</option>
+              <option>drainage</option>
+            </select>
+          </div>
+
+          <div className="input-box">
+            <span className="details">Full Name</span>
+            <input
+              type="text"
+              className="complaint-input"
+              placeholder="Enter name"
+              required
+            />
+          </div>
+
+          <div className="input-box">
+            <span className="details">Full Name</span>
+            <input
+              type="text"
+              className="complaint-input"
+              placeholder="Enter name"
+              required
+            />
+          </div>
+
+          <div className="input-box">
+            <span className="details">Full Name</span>
+            <input
+              type="text"
+              className="complaint-input"
+              placeholder="Enter name"
+              required
+            />
+          </div>
+
+          <div className="input-box">
+            <span className="details">Full Name</span>
+            <input
+              type="text"
+              className="complaint-input"
+              placeholder="Enter name"
+              required
+            />
+          </div>
+
+          <div className="input-box">
+            <span className="details">Full Name</span>
+            <input
+              type="text"
+              className="complaint-input"
+              placeholder="Enter name"
+              required
+            />
+          </div>
+
+          <div className="input-box">
+            <span className="details">Upload Proof</span>
+            <input type="file" className="complaint-proof" name="" id="" />
+          </div>
+          <div className="complaint-submit">
+            <input type="button" className="complaint-submit" value="Submit" />
           </div>
         </div>
-        <div className="newUserItem">
-          <label>Active</label>
-          <select className="newUserSelect" name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
-        </div>
-        <button className="newUserButton">Create</button>
       </form>
     </div>
   );
